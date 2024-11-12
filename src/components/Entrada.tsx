d71bffaa0466f217196b46e9121291d6e28adb72
Tiiito21 <alejandroacba03@gmail.com>
2024-11-12 12:12:31 +0100

    Fix Ganancias

--- src/components/Entrada.tsx
+++ src/components/Entrada.tsx
@@ -72,7 +72,7 @@ const Entrada = ({ tipoEntrada }: { tipoEntrada: string }) => {
           </button>
           <p className='text-xl text-center font-semibold'>TOTAL: {total}</p>
           <p className='text-xl text-center font-semibold'>DINERO: {dinero}</p>
-          <p className='text-xl text-center font-semibold'>GANANCIAS: {ganancias}</p>
+          <p className='text-xl text-center font-semibold'>GANANCIAS: {Math.trunc(ganancias * total)}</p>
           </ >
 
       </div>
